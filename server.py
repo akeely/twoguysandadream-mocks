2c92a79f3cfb8ce0b4ceb086587c93ea5b4d8281
Andrew Keely <andrew.keely@gmail.com>
2018-03-08 13:41:44 -0500

    Fix server. Add test league 27

--- server.py
+++ server.py
@@ -1,14 +1,18 @@
-import SimpleHTTPServer
-import SocketServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+import BaseHTTPServer
 
-PORT = 8080
 
-class Handler(SimpleHTTPServer.SimpleHTTPRequestHandler):
-    pass
+class CustomRequestHandler (SimpleHTTPRequestHandler):
+    def end_headers (self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Content-Type', 'application/json')
+        SimpleHTTPRequestHandler.end_headers(self)
 
-Handler.extensions_map[''] = 'application/json'
+def run(server_class=BaseHTTPServer.HTTPServer,
+        handler_class=CustomRequestHandler):
+    server_address = ('', 8080)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
-
-print "serving at port", PORT
-httpd.serve_forever()
+if __name__ == "__main__":
+    run()
